05c415e171e74a4006eee5c25470346fe6050c08
DiandreRuiz <Diandreruiz77@gmail.com>
2025-11-25 12:57:13 -0500

    Removed old monolithic structure old_app.py & implemented first route for /customers

--- app/blueprints/customers/routes.py
+++ app/blueprints/customers/routes.py
@@ -1,1 +1,16 @@
 from schemas import customer_schema, customers_schema
+from . import customers_bp
+from flask import request, jsonify
+from marshmallow import ValidationError
+from app.models import Customer
+from extensions import db
+from sqlalchemy import select
+
+@customers_bp.route("/", methods=["GET"])
+def get_customers():
+    query = select(Customer)
+    members = db.session.execute(query).scalars().all()
+    
+    return customers_schema.jsonify(members), 200
+
+
