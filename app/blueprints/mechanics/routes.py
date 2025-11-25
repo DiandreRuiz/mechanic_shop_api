abb00b13667b6e7bb0013e5ab0ab8d87b0e3ff52
DiandreRuiz <Diandreruiz77@gmail.com>
2025-11-25 14:48:33 -0500

    Fixed incorrect Model import & finished route for getting all mechanics entries

--- app/blueprints/mechanics/routes.py
+++ app/blueprints/mechanics/routes.py
@@ -3,10 +3,13 @@ from app.extensions import db
 from . import mechanics_bp
 from flask import request, jsonify
 from marshmallow import ValidationError
-from app.models import Customer
+from app.models import Mechanic
 from sqlalchemy import select
 from typing import Dict
 
 @mechanics_bp.route("/", methods=["GET"])
 def get_mechanics():
+    query = select(Mechanic)
+    mechanics = db.session.execute(query).scalars().all()
     
+    return mechanics_schema.jsonify(mechanics), 200
