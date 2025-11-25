243a56311ba397adf9b4ee32a5641dab794b042b
DiandreRuiz <Diandreruiz77@gmail.com>
2025-11-25 16:30:12 -0500

    Created __init__.py for tickets blueprint and created Marshmallow schemas for ticket & tickets from Ticket model

---
+++ app/blueprints/tickets/schemas.py
@@ -0,0 +1,9 @@
+from app.extensions import ma
+from app.models import Ticket
+
+class TicketSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Ticket
+        
+ticket_schema = TicketSchema()
+tickets_schema = TicketSchema(many=True)
