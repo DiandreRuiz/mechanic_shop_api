fd9c24e95732dd5288f14a499b804a69d52d3357
DiandreRuiz <Diandreruiz77@gmail.com>
2025-11-25 16:41:12 -0500

    Added route to tickets_bp blueprint for getting all ticket rows

---
+++ app/blueprints/tickets/routes.py
@@ -0,0 +1,17 @@
+from app.blueprints.tickets.schemas import ticket_schema, tickets_schema
+from app.extensions import db
+from . import tickets_bp
+from flask import request, jsonify
+from marshmallow import ValidationError
+from app.models import Ticket
+from sqlalchemy import select
+from typing import Dict
+
+@tickets_bp.route("/", methods=["GET"])
+def get_tickets():
+    query = select(Ticket)
+    tickets = db.session.execute(query).scalars().all()
+    
+    return tickets_schema.jsonify(tickets), 200
+
+    
