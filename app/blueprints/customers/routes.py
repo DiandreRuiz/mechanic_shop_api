a4099f20486e33e7d1840e240953e4a09ccca147
DiandreRuiz <Diandreruiz77@gmail.com>
2025-11-25 13:36:36 -0500

    Added a 'create customer' route for /customers blueprint

--- app/blueprints/customers/routes.py
+++ app/blueprints/customers/routes.py
@@ -13,4 +13,28 @@ def get_customers():
     
     return customers_schema.jsonify(members), 200
 
+@customers_bp.route("/", methods=["POST"])
+def add_customer():
+    # Validate request body
+    data = request.get_json()
+    if data is None:
+        return jsonify({"error": "Invalid or missing JSON body"}), 400
+    
+    # Validate request body schema
+    try:
+        customer_data = customer_schema.load(data)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+    
+    # Validate uniqueness of unique constrained columns
+    query = select(Customer).where(Customer.email == customer_data["email"])
+    existing_customer = db.session.execute(query).scalars().all()
+    if existing_customer:
+        return jsonify({"error": f"Account with email '{Customer.email}' already exists."})
 
+    # Validated, create new customer row
+    new_customer = Customer(**customer_data)
+    db.session.add(new_customer)
+    db.session.commit()
+    
+    return customer_schema.jsonify(new_customer), 201
