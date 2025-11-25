102f226ef309a913743edef2f352b33a869cea39
DiandreRuiz <Diandreruiz77@gmail.com>
2025-11-25 16:50:59 -0500

    Added route to tickets_bp blueprint for creation of a new row in the ticket table

--- app/blueprints/tickets/routes.py
+++ app/blueprints/tickets/routes.py
@@ -21,5 +21,25 @@ def get_ticket(ticket_id):
         return jsonify({"error": f"No ticket found with ticket_id: {ticket_id}"}), 404
     else:
         return ticket_schema.jsonify(ticket), 200
+    
+@tickets_bp.route("/", methods=["POST"])
+def create_ticket():
+    data = request.get_json()
+    if data is None:
+        return jsonify({"error": "Missing request JSON body"}), 400
+    
+    try:
+        ticket_data = ticket_schema.load(data)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+    
+    # Create new ticket & add
+    new_ticket = Ticket(**ticket_data)
+    db.session.add(new_ticket)
+    db.session.commit()
+    
+    return ticket_schema.jsonify(new_ticket), 201
+
+
 
     
