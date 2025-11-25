2cd6cc8f223df6faea4cb7485efeb57f7f284ba8
DiandreRuiz <Diandreruiz77@gmail.com>
2025-11-25 13:53:43 -0500

    Added route for updating customer via customer_id

--- app/blueprints/customers/routes.py
+++ app/blueprints/customers/routes.py
@@ -5,6 +5,7 @@ from flask import request, jsonify
 from marshmallow import ValidationError
 from app.models import Customer
 from sqlalchemy import select
+from typing import Dict
 
 @customers_bp.route("/", methods=["GET"])
 def get_customers():
@@ -48,4 +49,29 @@ def delete_customer(customer_id):
         db.session.delete(customer)
         db.session.commit()
     return jsonify({"message": f"Customer with customer_id: {customer_id} deleted"}), 200
+
+@customers_bp.route("/<int:customer_id>", methods=["PUT"])
+def update_customer(customer_id):
+    # 1. validate existance of customer
+    # 2. validate presence of data for update
+    # 3. validate schema of data for update
+    # 4. perform update of row
+    customer = db.session.get(Customer, customer_id)
+    if not customer:
+        return jsonify({"error": f"Could not find customer with customer_id of {customer_id}"}), 404
     
+    data = request.get_json()
+    if data is None:
+        return jsonify({"error": "Missing JSON body for request"}), 404
+    
+    try:
+        customer_data: Dict = customer_schema.load(data)
+    except ValidationError as e:
+        return jsonify({"error": f"malformed request body: {e.messages}"}), 400
+
+    # update all column values of pulled customer with Dict api
+    for k,v in customer_data.items():
+        setattr(customer, k, v)
+        
+    db.session.commit()
+    return customer_schema.jsonify(customer), 200
