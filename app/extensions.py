f0db38cd103bba9943ad77428e655babd5b22558
DiandreRuiz <Diandreruiz77@gmail.com>
2025-12-04 15:06:08 -0500

    Added flask limiter to extentions and created an instance of Limiter object as well with get_remote_address function used for the key_funct

--- app/extensions.py
+++ app/extensions.py
@@ -1,7 +1,10 @@
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from app.models import Base
 
 # Instantiations of extensions to be imported elsewhere
+limiter = Limiter(key_func=get_remote_address) # key_function = function returning domain to rate limit based on
 db = SQLAlchemy(model_class=Base)
 ma = Marshmallow()
