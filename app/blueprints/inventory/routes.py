d4dee259b9de007574a802475d4406c17fd3e698
DiandreRuiz <Diandreruiz77@gmail.com>
2025-12-19 17:27:32 -0500

    Added route for creation of new inventory item

--- app/blueprints/inventory/routes.py
+++ app/blueprints/inventory/routes.py
@@ -4,6 +4,7 @@ from sqlalchemy import select
 from app.models import Inventory
 from flask import request, jsonify
 from app.extensions import db
+from marshmallow import ValidationError
 
 @inventory_bp.route("/", methods=["GET"])
 def get_inventory_items():
@@ -17,6 +18,27 @@ def get_inventory_items():
         inventory_items = db.session.execute(query).scalars().all()
     
     return inventory_items_schema.jsonify(inventory_items), 200
+
+@inventory_bp.route("/", methods=["POST"])
+def add_inventory_item():
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "Missing post request body"}), 400
+    try:
+        inventory_item_data = inventory_item_schema.load()
+    except ValidationError as e:
+        return jsonify(e)
+    
+    inventory_name = inventory_item_data["name"]
+    query = select(Inventory).where(Inventory.name == inventory_name)
+    existing_inventory_item = db.session.execute(query).scalars().first()
+    if existing_inventory_item:
+        return jsonify({"error": f"Inventory item of name '{inventory_name}' already exists!"}), 400
+
+    new_inventory_item = Inventory(**inventory_item_data)
+    db.session.add(new_inventory_item)
+    db.session.commit()
+    return inventory_item_schema.jsonify(new_inventory_item), 201
     
     
     
