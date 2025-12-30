ca00ae822a7ad47f6099d9ac90aa98e17b1420d2
DiandreRuiz <Diandreruiz77@gmail.com>
2025-12-30 16:31:11 -0500

    Removed debugging comments

--- app.py
+++ app.py
@@ -3,14 +3,10 @@ print("starting imports")
 from app import create_app
 from app.extensions import db
 
-print("start")
 app = create_app("DevelopmentConfig")
-print("app created")
 
 # Create tables in metadata
 with app.app_context():
     db.create_all()
-    print("ORM tables created")
     
 app.run(debug=True, port=5001)
-print("App Ran")
