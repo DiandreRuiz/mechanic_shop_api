ed8f717b580107a56419603470b88c8a168a5e44
DiandreRuiz <Diandreruiz77@gmail.com>
2026-01-06 10:08:16 -0500

    Finished new test for login

--- tests/test_customer.py
+++ tests/test_customer.py
@@ -1,5 +1,7 @@
 from app import create_app
 from app.extensions import db
+from app.models import Customer
+from app.utils.util import encode_token
 import unittest
 
 class TestCustomer(unittest.TestCase):
@@ -11,7 +13,6 @@ class TestCustomer(unittest.TestCase):
         self.ctx.push()
         db.drop_all()
         db.create_all()
-        
         self.client = self.app.test_client()
             
     def tearDown(self):
@@ -40,3 +41,27 @@ class TestCustomer(unittest.TestCase):
         }
         response = self.client.post("/customers/", json=customer_payload)
         self.assertEqual(response.status_code, 400)
+        
+    def test_login(self):
+        # Add customer to test for
+        self.customer = Customer(
+            name="john pork",
+            phone="2224445555",
+            email="johnpork@gmail.com",
+            password="testpassword"
+        )
+        db.session.add(self.customer)
+        db.session.commit()
+        
+        # Generate access token
+        self.token = encode_token(self.customer.id)
+        
+        # Attempt login with token
+        credentials = {
+            "email": "johnpork@gmail.com",
+            "password": "testpassword"
+        }
+        response = self.client.post("/customers/login", json=credentials)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json['status'], "success")
+        return response.json['auth_token']
